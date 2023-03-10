1a2313972ee5729e5872a56dbc8680ddc89b105e
Wendesday36 <bertazoeanna@gmail.com>
2023-03-10 13:40:32 +0100

    atnevezes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-  const kepeklista = [
+  const KEPEKLISTA = [
     "kepek/DSC7025.JPG",
     "kepek/DSC7365.jpg",
     "kepek/DSC7444.jpg",
@@ -7,27 +7,24 @@ window.addEventListener("load", function () {
   ];
   let articleElem = document.querySelectorAll("article");
 
-  for (let index = 0; index < kepeklista.length; index++) {
-    articleElem[0].innerHTML += `<div><img src="${kepeklista[index]}" alt ="#"></div>`;
+  for (let index = 0; index < KEPEKLISTA.length; index++) {
+    articleElem[0].innerHTML += `<div><img src="${KEPEKLISTA[index]}" alt ="#"></div>`;
   }
   console.log(articleElem);
   articleElem.click;
-  const kiskepek = document.querySelectorAll("article div img");
-  const balgomb = document.querySelector(".bal");
-  const jobbgomb= document.querySelector(".jobb");
-  for (let index = 0; index < kiskepek.length; index++) {
-    kiskepek[index].addEventListener("click", function () {
-        kepmegnyit(index, kepeklista);
-      
+  const KISKEPEK = document.querySelectorAll("article div img");
+  for (let index = 0; index < KISKEPEK.length; index++) {
+    KISKEPEK[index].addEventListener("click", function () {
+      kepMegnyit(index, KEPEKLISTA);
     });
   }
-  
-  
+  const balgomb = document.querySelector(".bal");
+  const jobbgomb= document.querySelector(".jobb");
+  gombok = kiskepek;
 });
-function kepmegnyit(index, kepeklista) {
+function kepMegnyit(index, KEPEKLISTA) {
   console.log(event.target); /**ez az elem valtotta ki az esemenyt */
-  const nagykep = document.querySelectorAll("section div img");
-  console.log(nagykep);
-  nagykep[0].src = kepeklista[index];
-  jobbgomb = 
+  const NAGYKEP = document.querySelectorAll("section div img");
+  console.log(NAGYKEP);
+  NAGYKEP[0].src = KEPEKLISTA[index];
 }
